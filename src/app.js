c811c0dc29fa7739d226e873d9bd30880565e884
JNACHO2001 <113179500+JNACHO2001@users.noreply.github.com>
2025-08-12 18:32:40 -0500

    feat:tener tener cuenta la  ruta donde se dirije

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
-import express from 'express';
-import cors from 'cors';
-import usersRoutes from './routes/users.js';
+import express from "express";
+import cors from "cors";
+import usersRoutes from "./routes/users.js";
 
 const app = express();
 
-
-
+app.use(cors());
 app.use(express.json());
 
-app.use('/api/users', usersRoutes);
+app.use("/api/users", usersRoutes);
 
+app.use((req, res) => {
+  res.status(404).json({ message: "Ruta no encontrada" });
+});
+
+app.listen(3000, () => {
+  console.log(`Servidor corriendo en http://localhost:3000`);
+});
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,86 +1,89 @@
-import express from 'express';
-import conection from '../bd/db.js';
-import cors from "cors"
+import express from "express";
+import conection from "../bd/db.js";
 
-const router = express();
-router.use(cors());
+const router = express.Router();
 
-router.use(express.json());
-
-router.get('/', (req, res) => {
-  const sql = 'SELECT *FROM users';
+router.get("/", (req, res) => {
+  const sql = "SELECT * FROM users";
   conection.query(sql, (err, results) => {
     if (err) {
       console.error(err);
-      return res.status(500).json({ message: 'Error obteniendo users' });
+      return res.status(500).json({ message: "Error obteniendo users" });
     }
     res.json(results);
-    
   });
 });
 
-
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   const { id } = req.params;
-  const sql = 'SELECT * FROM users WHERE id = ?';
+  const sql = "SELECT * FROM users WHERE id = ?";
   conection.query(sql, [id], (err, results) => {
     if (err) {
       console.error(err);
-      return res.status(500).json({ message: 'Error obteniendo user' });
+      return res.status(500).json({ message: "Error obteniendo user" });
     }
     if (results.length === 0) {
-      return res.status(404).json({ message: 'user no encontrado' });
+      return res.status(404).json({ message: "user no encontrado" });
     }
     res.json(results[0]);
   });
 });
 
-
-router.post('/', (req, res) => {
-  const { name, identiti, addres,phone,email,plataform } = req.body;
-  const sql = 'INSERT INTO users (name_user, identiti,addres,phone,email,plataform) VALUES (?, ?, ?,?,?,?)';
-  conection.query(sql, [name, identiti, addres,phone,email,plataform ], (err, result) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ message: 'Error creando user' });
+router.post("/", (req, res) => {
+  const { name, identiti, addres, phone, email, plataform } = req.body;
+  const sql =
+    "INSERT INTO users (name_user, identiti,addres,phone,email,plataform) VALUES (?, ?, ?,?,?,?)";
+  conection.query(
+    sql,
+    [name, identiti, addres, phone, email, plataform],
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ message: "Error creando user" });
+      }
+      res.status(201).json({ message: "user creado", id: result.insertId });
     }
-    res.status(201).json({ message: 'user creado', id: result.insertId });
-  });
+  );
 });
 
-
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   const { id } = req.params;
-  const { name_user, identiti, addres,phone,email,plataform  } = req.body;
-  const sql = 'UPDATE users SET name_user = ?, identiti = ?, addres = ?,phone=?,email=? , plataform=?  WHERE id = ?';
-  conection.query(sql, [name_user, identiti, addres,phone,email,plataform,id], (err, result) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ message: 'Error actualizando user' });
+  const { name_user, identiti, addres, phone, email, plataform } = req.body;
+  const sql =
+    "UPDATE users SET name_user = ?, identiti = ?, addres = ?,phone=?,email=? , plataform=?  WHERE id = ?";
+  conection.query(
+    sql,
+    [name_user, identiti, addres, phone, email, plataform, id],
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ message: "Error actualizando user" });
+      }
+      if (result.affectedRows === 0) {
+        return res
+          .status(404)
+          .json({ message: "user no encontrado para actualizar" });
+      }
+      res.json({ message: "user actualizado" });
     }
-    if (result.affectedRows === 0) {
-      return res.status(404).json({ message: 'user no encontrado para actualizar' });
-    }
-    res.json({ message: 'user actualizado' });
-  });
+  );
 });
 
-
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   const { id } = req.params;
-  const sql = 'DELETE FROM users WHERE id = ?';
+  const sql = "DELETE FROM users WHERE id = ?";
   conection.query(sql, [id], (err, result) => {
     if (err) {
       console.error(err);
-      return res.status(500).json({ message: 'Error eliminando user' });
+      return res.status(500).json({ message: "Error eliminando user" });
     }
     if (result.affectedRows === 0) {
-      return res.status(404).json({ message: 'user no encontrado para eliminar' });
+      return res
+        .status(404)
+        .json({ message: "user no encontrado para eliminar" });
     }
-    res.json({ message: 'user eliminado' });
+    res.json({ message: "user eliminado" });
   });
 });
 
-router.listen(3000, () => {
-  console.log(`Servidor corriendo en http://localhost:3000`);
-});
+export default router;
