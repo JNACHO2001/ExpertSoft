3ff125056d0ae52acfd85e9bf9aca8f00c632714
JNACHO2001 <113179500+JNACHO2001@users.noreply.github.com>
2025-08-12 08:02:23 -0500

    feat:database created

--- src/bd/db.js
+++ src/bd/db.js
@@ -1,3 +1,25 @@
+
+
 import mysql from "mysql2"
+
+
+const conection = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"root",
+    database:"pd_jose_gomez_tayrona"
+
+})
+
+conection.connect((error)=>{
+    if (error) {
+        console.log("no se pudo conectar",error)
+        
+    }
+    console.log("conectada")
+    
+}
+
+)
 
 
